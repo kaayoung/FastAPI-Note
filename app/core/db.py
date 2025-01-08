a500d6df3a93c5922a314b0293e21f024f8b8b75
kaayoung <2catailna@naver.com>
2025-01-09 03:16:25 +0900

    feat : env 처리

--- app/core/db.py
+++ app/core/db.py
@@ -5,7 +5,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLITE_DB_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False}) 
 ## 데이터베이스와 연결을 설정하기 위해 사용
--- app/domain/user/user_router.py
+++ app/domain/user/user_router.py
@@ -4,11 +4,15 @@ from sqlalchemy.orm import Session
 from app.core.db import  get_db
 from app.domain.user import user_schema, user_crud
 from datetime import datetime 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 router = APIRouter(prefix="/api/user")
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
-SECRET_KEY = "4ab2fce7a6bd79e1c014396315ed322dd6edb1c5d975c6b74a2904135172c03c"
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
 
